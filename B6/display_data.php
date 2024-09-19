fb7079c769af51b9dd3822bd07dd71841a886e4c
ltth268 <152267588+ltth268@users.noreply.github.com>
2024-09-19 18:31:44 +0700

    Add files via upload

---
+++ B6/display_data.php
@@ -0,0 +1,42 @@
+<?php
+$servername = "localhost:8111";
+$username = "root";
+$password = "";
+$dbname = "b5_mydb";
+
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    // Truy vấn để lấy dữ liệu từ bảng MyGuests
+    $stmt = $conn->prepare("SELECT id, firstname, lastname, email, reg_date FROM MyGuests");
+    $stmt->execute();
+    
+    // Thiết lập chế độ lấy dữ liệu dưới dạng associative array
+    $result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
+    $rows = $stmt->fetchAll();
+    ?>
+    <table border="1">
+        <tr>
+            <th>Id</th>
+            <th>Firstname</th>
+            <th>Lastname</th>
+            <th>Email</th>
+            <th>Reg Date</th>
+        </tr>
+        <?php foreach($rows as $row) { ?>
+        <tr>
+            <td><?php echo $row['id']; ?></td>
+            <td><?php echo $row['firstname']; ?></td>
+            <td><?php echo $row['lastname']; ?></td>
+            <td><?php echo $row['email']; ?></td>
+            <td><?php echo $row['reg_date']; ?></td>
+        </tr>
+        <?php } ?>
+    </table>
+    <?php
+} catch (PDOException $e) {
+    echo "Lỗi: " . $e->getMessage();
+}
+$conn = null;
+?>
